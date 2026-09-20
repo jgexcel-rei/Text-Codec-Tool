68bc7ad3b746dbff4391821c2927a71acecee082
jgexcel-rei <azje2894@gmail.com>
2026-09-20 17:11:10 +0800

    标准摩斯密码编码解码器

---
+++ RTO5.c
@@ -0,0 +1,72 @@
+﻿// 凯撒移位密码ROT5
+#include <stdio.h>
+#include <string.h>
+int lock(char a[], int b)
+{
+  for (int i = 0; i < b; i++)
+  {
+    if ((a[i] >= 'a' && a[i] <= 'z') || (a[i] >= 'A' && a[i] <= 'Z'))
+    {
+      if ((a[i] >= 'a' && a[i] + 5 <= 'z') || (a[i] >= 'A' && a[i] + 5 <= 'Z'))
+      {
+        a[i] = a[i] + 5;
+      }
+      else
+      {
+        a[i] = a[i] - 21;
+      }
+    }
+    else
+    {
+      return EOF;
+    }
+  }
+  return 0;
+}
+void unlock(char a[], int b)
+{
+  for (int i = 0; i < b; i++)
+  {
+    if (a[i] >= 'a' && a[i] <= 'z')
+    {
+
+      if (a[i] - 5 >= 'a')
+      {
+        a[i] = a[i] - 5;
+      }
+      else
+      {
+        a[i] = a[i] + 21;
+      }
+    }
+    if (a[i] >= 'A' && a[i] <= 'Z')
+    {
+      if (a[i] - 5 >= 'A')
+      {
+        a[i] = a[i] - 5;
+      }
+      else
+      {
+        a[i] = a[i] + 21;
+      }
+    }
+  }
+}
+int main()
+{
+  char a[100];
+  printf("请输入密码：");
+  scanf("%s", a);
+  int b = strlen(a);
+  if (lock(a, b) == EOF)
+  {
+    goto END;
+  }
+  printf("加密后的密码为：%s\n", a);
+  unlock(a, b);
+  printf("解密后的密码为：%s\n", a);
+  return 0;
+END:
+  printf("密码中包含非字母字符，无法加密或解密\n");
+  return 0;
+}
